c370bc17373e3b464dd2d4752e0b76887bb29c9a
swata-dev <74521093+swata-dev@users.noreply.github.com>
2021-04-05 15:40:59 +0900

    product_model_migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_05_063017) do
+ActiveRecord::Schema.define(version: 2021_04_05_064025) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "purchasers", force: :cascade do |t|
     t.string "name", null: false
