6988a1a5f7b15ae8e02ef1058137abec6754bb05
swata-dev <74521093+swata-dev@users.noreply.github.com>
2021-04-06 19:25:31 +0900

    formatting_response

--- app/controllers/purchasers_controller.rb
+++ app/controllers/purchasers_controller.rb
@@ -12,11 +12,18 @@ class PurchasersController < ApplicationController
     # set date-range for filtering
     start_date = params[:start_date]
     end_date = params[:end_date]
+    # find all products purchased by the purchaser during the range
+    purchaser = Purchaser.find(params[:id])
+    products = purchaser.purchaser_products.where(purchase_date: start_date..end_date).order(purchase_timestamp: 'DESC')
+    # formatting for response
+    purchases = {}
+    purchases[:purchases] = {}
+    products.each do |product|
+      product_name = {"product": product.product.name}
+      purchases[:purchases].store(product[:purchase_date], [product_name])
+    end
 
-    @purchaser = Purchaser.find(params[:id])
-    @products = @purchaser.purchaser_products.where(purchase_date: start_date..end_date).order(purchase_timestamp: 'DESC')
-
-    render json: @products
+    render json: purchases
   end
 
   private
